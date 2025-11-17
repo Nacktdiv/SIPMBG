55a5181eba11765efccc60050d0806a5d448bda1
Nacktdiv <nadhifinformatika66@gmail.com>
2025-11-17 22:41:59 +0700

    menyelesaikan bagian navbar dan hero pada halaman beranda

--- client/src/component/navbar.jsx
+++ client/src/component/navbar.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import {useState} from "react";
 import logo from '../assets/logo.png';
 import { IoMenu } from "react-icons/io5"
 
 function Navbar () {
+    const [statusMenu, setStatusMenu] = useState(false)
+    console.log(statusMenu)
+
     return (
-        <nav className="width-full h-[56px] flex bg-[#4E5F71] px-[160px] py-[8px] justify-between ">
+        <nav className={`w-full h-[56px] flex bg-[#4E5F71] px-[16px] md:px-[160px] py-[8px] justify-between fixed`}>
             <img src={logo} alt="logo" className="" />
-            <div className=" flex gap-[20px] font-[--font-family-poppins] font-[var(--font-weight-bold)]">
-                <ul className="text-white text-base flex items-center gap-[20px]">
-                    <li>Beranda</li>
-                    <li>Lacak Mbg</li>
-                    <li>Kontak</li>
-                </ul>
-                <button className="bg-[#0DC6BF] px-2"><p className="text-base ">Masuk Dashboard</p></button>
+            <div className={` flex gap-[20px] font-[--font-family-poppins] font-[var(--font-weight-bold)] items-center  `}>
+                    <IoMenu className="text-white text-2xl md:hidden" onClick={() => setStatusMenu(!statusMenu)} />
+                    <div className={`${statusMenu ? 'bg-[#4E5F71] absolute top-[100%] right-[0px] w-max p-0' : 'hidden'}  md:flex md:flex-row gap-[20px] items-center md:h-full `}>
+                        <ul className={`text-white text-base flex ${statusMenu ? 'flex-col w-full' : 'flex-row '} items-center judtify-center gap-[20px] `}>
+                            <li>Beranda</li>
+                            <li>Lacak Mbg</li>
+                            <li>Kontak</li>
+                        </ul>
+                        <button className="bg-[#0DC6BF] mt-[20px] md:mt-[0px] px-[4px] py-[4px] block"><p className="text-base ">Masuk Dashboard</p></button>
+                    </div>
+                  
             </div>
         </nav>
     )
--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import Navbar from "./component/navbar";
 import { IoMenu } from "react-icons/io5"
+import Bocah from "./assets/gambar-anak.png"
 
 function Home() {
     return (
-        <div>
+        <>
             <Navbar />
-            <IoMenu className="text-2xl" />
-            <h2>Welcome to the Home Page</h2>
-            <p>This is the main content of the home page.</p>
-        </div>
+            <div className="pt-14">
+                <main>
+                    <section id="section-1" className="grid-container h-[839] md:h-[544px] flex flex-col md:flex-row  items-center">
+                        <div className=" col-span-4 md:col-span-6 order-1 md:order-0 flex flex-col justify-center gap-[24px]">
+                            <div className="flex flex-col gap-[16px]">
+                                <h1 className="text-4xl font-[--font-family-poppins] font-[var(--font-weight-bold)]">Solusi Digital Transparan untuk Distribusi & Pengelolaan Makanan Bergizi Gratis</h1>
+                                <p className="text-xl font-[--font-family-poppins] font-[var(--font-weight-regular)]">Dapatkan informasi detail mengenai penerima, jadwal, dan pelaporan setiap alokasi makanan bergizi gratis.</p>
+                            </div>
+                            <div className="flex justify-center gap-[20px]">
+                                <button className="bg-[#0DC6BF] rounded-[20px] py-2 px-10"><p className="text-base text-white font-[--font-family-poppins] font-[var(--font-weight-regular)]">Lacak</p></button>
+                                <button className="border-2 border-solid rounded-[20px] p-2"><p className="text-base font-[--font-family-poppins] font-[var(--font-weight-regular)]">Masuk Dashboard</p></button>
+                            </div>
+                        </div>
+                        <div className="col-span-4 md:col-span-6 order-0 md:order-1 h-[380px] flex justify-center items-center">
+                            <img src={Bocah} alt="Gambar anak" className="col-span-6 w-[75%] h-[75%]"/>
+                        </div>
+                    </section>
+                </main>
+            </div>
+        </>
     )
 }
 
