da1a004830d15be51bda8d8c787cb1cfff4e102e
Ahya Adib <ahyaadib123@gmail.com>
2025-11-22 10:06:48 +0700

    cm1

--- client/src/component/navbar.jsx
+++ client/src/component/navbar.jsx
@@ -13,7 +13,7 @@ function Navbar () {
                     <IoMenu className="text-white text-2xl md:hidden" onClick={() => setStatusMenu(!statusMenu)} />
                     <div className={`${statusMenu ? 'bg-[#4E5F71] absolute top-[100%] right-[0px] w-max p-0' : 'hidden'}  md:flex md:flex-row gap-[20px] items-center md:h-full `}>
                         <ul className={`text-cream text-base flex ${statusMenu ? 'flex-col w-full' : 'flex-row '} items-center judtify-center gap-[20px] `}>
-                            <li><Link to="/">Beranda</Link></li>
+                            <li><Link to="/home">Beranda</Link></li>
                             <li><Link to="/lacakmbg">Lacak MBG</Link></li>
                             <li><Link to="/kontak">Kontak</Link></li>
                             <li><Link to="/testai">Test Inference</Link></li>
